0fcc9cd21d68d025bddc34d84117124ee94870b8
parvati <pitambarthapa288@gmail.com>
2021-06-19 18:09:32 +0530

    cheking on git

--- src/demo/testing.java
+++ src/demo/testing.java
@@ -1,5 +1,15 @@
 package demo;
 
 public class testing {
+	public static void main(String a[])
+	{
+		System.out.println("testing.....");
+		System.out.println("working!!!!!");
+		
+		System.out.println("testing on git");
+		System.out.println("working!!!!");
 
+		
+	}
+	
 }
